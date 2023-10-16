bf30ce5a71f4f4bd3412dd45a7f0847a2cbdd34a
Yutak_Seo <soano2428@gmail.com>
2023-10-16 20:48:58 +0900

    23.10.16

--- radious_sensor.py
+++ radious_sensor.py
@@ -1,8 +1,44 @@
-class radiusSensor:
-    def __init__(self, cor_x, cor_y, radius):
-        self.x = cor_x
-        self.y = cor_y
+class Sensor:
+    global r_grid
+    def __init__(self, x,y, radius):
+        self.core_x = x
+        self.core_y = y
         self.radius = radius
-    
-    
-    
+         
+        r_grid = self.radius // 5
+        
+    def make_coverage(grid):
+        grid[x-2][y+2] == 1
+        grid[x-1][y+2] == 1
+        grid[x][y+2] == 1
+        grid[x+1][y+2] == 1
+        grid[x+2][y+2] == 1
+         
+        grid[x-2][y+1] == 1
+        grid[x-1][y+1] == 1
+        grid[x][y+1] == 1
+        grid[x+1][y+1] == 1
+        grid[x+2][y+1] == 1
+        
+        grid[x-2][y] == 1
+        grid[x-1][y] == 1
+        grid[x][y] == 1
+        grid[x+1][y] == 1
+        grid[x+2][y] == 1
+         
+        grid[x-2][y-1] == 1
+        grid[x-1][y-1] == 1
+        grid[x][y-1] == 1
+        grid[x+1][y-1] == 1
+        grid[x+2][y-1] == 1
+        
+        grid[x-2][y-2] == 1
+        grid[x-1][y-2] == 1
+        grid[x][y-2] == 1
+        grid[x+1][y-2] == 1
+        grid[x+2][y-2] == 1
+        
+        grid[x][y+3] == 1
+        grid[x-3][y] == 1
+        grid[x+3][y] == 1
+        grid[x][y-3] == 1

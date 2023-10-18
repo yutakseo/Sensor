5509f087a0a39a575317592743c90f12c471faee
Yutak_Seo <soano2428@gmail.com>
2023-10-18 21:03:59 +0900

    23.10.18 추가 버그 수정

--- sensor_module.py
+++ sensor_module.py
@@ -30,28 +30,33 @@ class Sensor:
                 y_length = self.sensor_position[1] - i      #세로축 길이
                 cell_length = math.sqrt(pow(x_length, 2) + pow(y_length, 2))
 
-                if cell_length < sensor_range:
+                if cell_length <= sensor_range:
                     data[i][j] += 10
                     
         return data
     
 
 #<test_data>
-sensor_range = 1
-data = [[1,1,1],
-        [1,1,1],
-        [1,1,1]]
-
+sensor_range = 2
+data = [[1,1,1,1,1,1],
+        [1,1,1,1,1,1],
+        [1,1,1,1,1,1],
+        [1,1,1,1,1,1],
+        [1,1,1,1,1,1],
+        [1,1,1,1,1,1]
+       ]
+'''
 #인스턴스1 생성 - 센서(2,2)
-sensor_position = (2,2)
+sensor_position = (5,5)
 test_instance = Sensor(data, sensor_position, sensor_range)
 #print(test_instance.deploy_sensor())
 data = test_instance.deploy_sensor()
-
+'''
 #인스턴스2 생성 - 센서(0,0)
 sensor_position = (0,0)
 test_instance2 = Sensor(data, sensor_position, sensor_range)
 #print(test_instance2.deploy_sensor())
 data = test_instance2.deploy_sensor()
+
 #결과
 print(data)

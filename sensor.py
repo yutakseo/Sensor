c4eababff5af5d621ecaba0440a4c4a48e1bf23f
Yutak_Seo <soano2428@gmail.com>
2023-10-19 10:53:24 +0900

    23.10.19 수정완

--- sensor.py
+++ sensor.py
@@ -1,22 +1,9 @@
 import math
 from sample_data import *
+from createArray import *
+from TEST_DATASET import *
 
-'''
-sensor_range = 2
-sensor_position = (0,0)
-data = sample_data1
 
-for i in range(len(data)):
-    for j in range(len(data[0])):
-        x_length = sensor_position[0] - j #가로축 길이
-        y_length = sensor_position[1] - i      #세로축 길이
-        cell_length = math.sqrt(pow(x_length, 2) + pow(y_length, 2))
-        
-        if  cell_length < sensor_range:
-            data[i][j] += 1
-
-print(data)
-'''
 class Sensor:
     def __init__(self, map_data, sensor_position, coverage):
         self.map_data = map_data
@@ -31,32 +18,29 @@ class Sensor:
                 cell_length = math.sqrt(pow(x_length, 2) + pow(y_length, 2))
 
                 if cell_length <= sensor_range:
-                    data[i][j] += 10
+                    data_map[i][j] += 10
                     
-        return data
+        return data_map
     
 
 #<test_data>
-sensor_range = 2
-data = [[1,1,1,1,1,1],
-        [1,1,1,1,1,1],
-        [1,1,1,1,1,1],
-        [1,1,1,1,1,1],
-        [1,1,1,1,1,1],
-        [1,1,1,1,1,1]
-       ]
-'''
+sensor_range = 30
+data_map = rectangle_140by140
+
+
+
 #인스턴스1 생성 - 센서(2,2)
-sensor_position = (5,5)
-test_instance = Sensor(data, sensor_position, sensor_range)
+sensor_position = (50,46)
+test_instance = Sensor(data_map, sensor_position, sensor_range)
 #print(test_instance.deploy_sensor())
-data = test_instance.deploy_sensor()
+data_map = test_instance.deploy_sensor()
 '''
 #인스턴스2 생성 - 센서(0,0)
-sensor_position = (0,0)
+sensor_position = (90,70)
 test_instance2 = Sensor(data, sensor_position, sensor_range)
 #print(test_instance2.deploy_sensor())
 data = test_instance2.deploy_sensor()
-
+'''
 #결과
-print(data)
+visual_array("결과", data_map)
+

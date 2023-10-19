feeab141822f8f17154783567eec89a0acaa1e55
Yutak_Seo <soano2428@gmail.com>
2023-10-19 13:43:38 +0900

    23.10.19 Sensor 모듈 리팩터링

---
+++ PlayGroundMain.py
@@ -0,0 +1,29 @@
+from Sensor import *
+import math, os, sys
+
+__file__ = os.getcwd()
+__root__ = os.path.dirname(__file__)
+
+mapdata_dir_path = os.path.join(__root__,"MapData")
+sys.path.append(mapdata_dir_path)
+
+from TEST_DATASET import *
+from matplotlib import pyplot as plt
+
+
+
+
+data_map = rectangle_140by140
+
+
+#인스턴스1 생성
+test_instance = Sensor(data_map, (30,50), 20)
+test_instance.deploy_sensor()
+#인스턴스2 생성
+test_instance2 = Sensor(data_map, (100,30), 12)
+test_instance2.deploy_sensor()
+
+
+#결과 출력
+plt.imshow(data_map, cmap='jet')
+plt.show()
--- sensor.py
+++ sensor.py
@@ -1,8 +1,14 @@
-import math
-from sample_data import *
-from createArray import *
+import math, os, sys
+
+__file__ = os.getcwd()
+__root__ = os.path.dirname(__file__)
+
+mapdata_dir_path = os.path.join(__root__,"MapData")
+sys.path.append(mapdata_dir_path)
+
 from TEST_DATASET import *
 from matplotlib import pyplot as plt
+
 
 class Sensor:
     def __init__(self, map_data, sensor_position, coverage):
@@ -17,31 +23,13 @@ class Sensor:
                 y_length = self.sensor_position[1] - i      #세로축 길이
                 cell_length = math.sqrt(pow(x_length, 2) + pow(y_length, 2))
 
-                if cell_length <= sensor_range:
-                    data_map[i][j] += 10
+                if cell_length <= self.coverage:
+                    self.map_data[i][j] += 10
                     
-        return data_map
+        return self.map_data
     
 
-#<test_data>
-sensor_range = 6
-data_map = truncated_140by140
 
 
 
-#인스턴스1 생성 - 센서(2,2)
-sensor_position = (30,50)
-test_instance = Sensor(data_map, sensor_position, sensor_range)
-#print(test_instance.deploy_sensor())
-data_map = test_instance.deploy_sensor()
 
-#인스턴스2 생성 - 센서(0,0)
-sensor_position = (90,70)
-test_instance2 = Sensor(data_map, sensor_position, sensor_range)
-#print(test_instance2.deploy_sensor())
-data = test_instance2.deploy_sensor()
-
-
-#결과 출력
-plt.imshow(data_map, cmap='jet')
-plt.show()
